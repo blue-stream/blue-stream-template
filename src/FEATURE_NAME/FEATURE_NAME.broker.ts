ac3fda4d847592756774c3840650044e00c9a2dd
SirSaar <saar643@gmail.com>
2018-10-24 17:33:14 +0300

    fix(rabbitmq): add await

--- src/FEATURE_NAME/FEATURE_NAME.broker.ts
+++ src/FEATURE_NAME/FEATURE_NAME.broker.ts
@@ -6,11 +6,11 @@ export class FeatureNameBroker {
     static broker: RabbitMQ = new RabbitMQ('application', 'topic');
 
     public static async startBrokers() {
-        FeatureNameBroker.broker.start();
+        await FeatureNameBroker.broker.start();
     }
 
     public static async subscribe() {
-        FeatureNameBroker.broker.subscribe('source.action.status', (message: string) => {
+        await FeatureNameBroker.broker.subscribe('source.action.status', (message: string) => {
             console.log('example publish message...');
             FeatureNameBroker.broker.publish('Source.Action.Status', `we recieved this: ${message}`);
         });
