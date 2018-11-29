9e0738592ebbe76da35c15130a1c7e812df0c773
Yuval Assa <assa.yuval107@gmail.com>
2018-11-29 14:30:50 +0200

    style: change from backticks to normal quotes

--- src/utils/errors/userErrors.ts
+++ src/utils/errors/userErrors.ts
@@ -2,18 +2,18 @@ import { UserError } from './applicationError';
 
 export class PropertyInvalidError extends UserError {
     constructor(message?: string) {
-        super(message || `Property is invalid`, 400);
+        super(message || 'Property is invalid', 400);
     }
 }
 
 export class IdInvalidError extends UserError {
     constructor(message?: string) {
-        super(message || `Id is invalid`, 400);
+        super(message || 'Id is invalid', 400);
     }
 }
 
 export class FeatureNameNotFoundError extends UserError {
     constructor(message?: string) {
-        super(message || `FeatureName not found`, 404);
+        super(message || 'FeatureName not found', 404);
     }
 }
